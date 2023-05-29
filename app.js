951b7f8c738a715f224536bba3d1324b3f9d0eff
Burak Unlu <burakaliunlu@gmail.com>
2023-05-28 21:33:02 -0400

    merged all files from local project to remote repo

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 const port = 3000;
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+// Serve static files from the "public" directory
+app.use(express.static(__dirname + '/public'));
+
+let todos = ['Task 1', 'Task 2', 'Task 3'];
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get('/todos', (req, res) => {
+  // Render a view to display the todo list
+  res.render('todos', { todos });
+});
+
+app.post('/todos', (req, res) => {
+  const newTodo = req.body.todo;
+  todos.push(newTodo);
+  res.redirect('/todos');
+});
+
+app.post('/todos/delete', (req, res) => {
+  const todoIndex = req.body.index;
+  todos.splice(todoIndex, 1);
+  res.redirect('/todos');
+});
+
+
+//start the server and listen for incoming requests:
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
